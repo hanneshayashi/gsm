bce36245433745654675ce3b23a0b2c8f5d539f3
Hannes Hayashi <hannes.siefert@gmail.com>
2020-12-19 09:51:54 +0100

    add functions to show and clear the current log

---
+++ cmd/log_show.go
@@ -0,0 +1,46 @@
+/*
+Package cmd contains the commands available to the end user
+Copyright © 2020 Hannes Hayashi
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"log"
+
+	"github.com/hanneshayashi/gsm/gsmhelpers"
+	"github.com/hanneshayashi/gsm/gsmlog"
+
+	"github.com/spf13/cobra"
+)
+
+// logShowCmd represents the show command
+var logShowCmd = &cobra.Command{
+	Use:               "show",
+	Short:             "Shows the current log.",
+	Long:              "",
+	DisableAutoGenTag: true,
+	Run: func(cmd *cobra.Command, args []string) {
+		flags := gsmhelpers.FlagsToMap(cmd.Flags())
+		err := gsmlog.PrintLastLines(logFile, flags["lines"].GetInt())
+		if err != nil {
+			log.Fatalf("Error showing log: %v", err)
+		}
+	},
+}
+
+func init() {
+	gsmhelpers.InitCommand(logCmd, logShowCmd, logFlags)
+}
---
+++ gsmlog/general.go
@@ -0,0 +1,53 @@
+/*
+Package gsmlog implements the Enterprise License Manager API
+Copyright © 2020 Hannes Hayashi
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package gsmlog
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+// PrintLastLines prints the last n lines in the specified file
+func PrintLastLines(path string, n int) error {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	s := string(b)
+	lines := strings.Split(s, "\n")
+	max := len(lines) - 1
+	if n > max {
+		n = max
+	}
+	for _, l := range lines[max-n : max] {
+		fmt.Println(l)
+	}
+	return nil
+}
+
+// Clear clears the specified file (truncate its content)
+func Clear(path string) error {
+	f, err := os.OpenFile(path, os.O_TRUNC, os.ModeTemporary)
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
