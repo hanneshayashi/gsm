d4762b5ae3d41dec79b105c952afc09d23ac17b8
Hannes Hayashi <hannes.siefert@gmail.com>
2021-01-10 07:35:13 +0100

    print go version during build

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ mkdir gsm/linux-x64
 mkdir gsm/linux-arm64
 mkdir gsm/mac-x64
 
+go version
+
 GOARCH=amd64 GOOS=windows go build -o gsm/win-x64/gsm.exe
 GOARCH=amd64 GOOS=linux go build -o gsm/linux-x64/gsm
 GOARCH=arm64 GOOS=linux go build -o gsm/linux-arm64/gsm
