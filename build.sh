6e440d0b66701af36ae69ef602c4cde153b9e3e2
Hannes Hayashi <hannes.siefert@gmail.com>
2021-02-17 16:48:37 +0100

    rename x64 builds to amd64 and add mac-arm64 builds

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 mkdir gsm
-mkdir gsm/win-x64
-mkdir gsm/linux-x64
+mkdir gsm/win-amd64
+mkdir gsm/linux-amd64
 mkdir gsm/linux-arm64
-mkdir gsm/mac-x64
+mkdir gsm/mac-amd64
+mkdir gsm/mac-arm64
 
 go version
 
-GOARCH=amd64 GOOS=windows go build -o gsm/win-x64/gsm.exe
-GOARCH=amd64 GOOS=linux go build -o gsm/linux-x64/gsm
+GOARCH=amd64 GOOS=windows go build -o gsm/win-amd64/gsm.exe
+GOARCH=amd64 GOOS=linux go build -o gsm/linux-amd64/gsm
 GOARCH=arm64 GOOS=linux go build -o gsm/linux-arm64/gsm
-GOARCH=amd64 GOOS=darwin go build -o gsm/mac-x64/gsm
+GOARCH=amd64 GOOS=darwin go build -o gsm/mac-amd64/gsm
+GOARCH=arm64 GOOS=darwin go build -o gsm/mac-arm64/gsm
--- gsutil.sh
+++ gsutil.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-cp ./LICENSE gsm/win-x64/
-cp ./LICENSE gsm/linux-x64/
+cp ./LICENSE gsm/win-amd64/
+cp ./LICENSE gsm/linux-amd64/
 cp ./LICENSE gsm/linux-arm64/
-cp ./LICENSE gsm/mac-x64/
+cp ./LICENSE gsm/mac-amd64/
+cp ./LICENSE gsm/mac-arm64/
 
 mkdir tmp
 
-tar -czf tmp/gsm_win-x64.tar.gz -C gsm/win-x64 .
-tar -czf tmp/gsm_linux-x64.tar.gz -C gsm/linux-x64 .
+tar -czf tmp/gsm_win-amd64.tar.gz -C gsm/win-amd64 .
+tar -czf tmp/gsm_linux-amd64.tar.gz -C gsm/linux-amd64 .
 tar -czf tmp/gsm_linux-arm64.tar.gz -C gsm/linux-arm64 .
-tar -czf tmp/gsm_mac-x64.tar.gz -C gsm/mac-x64 .
+tar -czf tmp/gsm_mac-amd64.tar.gz -C gsm/mac-amd64 .
+tar -czf tmp/gsm_mac-arm64.tar.gz -C gsm/mac-arm46 .
 
 gsutil -m cp tmp/* gs://build-arts/gsm/
 
-gsutil setmeta -h 'Content-Disposition:filename=gsm_win-x64.tar.gz' gs://build-arts/gsm/gsm_win-x64.tar.gz
-gsutil setmeta -h 'Content-Disposition:filename=gsm_linux-x64.tar.gz' gs://build-arts/gsm/gsm_linux-x64.tar.gz
+gsutil setmeta -h 'Content-Disposition:filename=gsm_win-amd64.tar.gz' gs://build-arts/gsm/gsm_win-amd64.tar.gz
+gsutil setmeta -h 'Content-Disposition:filename=gsm_linux-amd64.tar.gz' gs://build-arts/gsm/gsm_linux-amd64.tar.gz
 gsutil setmeta -h 'Content-Disposition:filename=gsm_linux-arm64.tar.gz' gs://build-arts/gsm/gsm_linux-arm64.tar.gz
-gsutil setmeta -h 'Content-Disposition:filename=gsm_mac-x64.tar.gz' gs://build-arts/gsm/gsm_mac-x64.tar.gz
+gsutil setmeta -h 'Content-Disposition:filename=gsm_mac-amd64.tar.gz' gs://build-arts/gsm/gsm_mac-amd64.tar.gz
+gsutil setmeta -h 'Content-Disposition:filename=gsm_mac-arm64.tar.gz' gs://build-arts/gsm/gsm_mac-arm64.tar.gz
 
 # gsutil -m acl ch -r -g AllUsers:R gs://build-arts/gsm/*
