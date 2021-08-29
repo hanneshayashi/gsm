137296d3cfe6e83c32c4e98b92749226dfbd3abd
Hannes Hayashi <26571811+hanneshayashi@users.noreply.github.com>
2021-08-29 07:51:20 +0200

    set version to v0.4.0-dev

--- cmd/root.go
+++ cmd/root.go
@@ -131,7 +131,7 @@ For documentation see https://gsm.hayashi-ke.online.`,
 			log.Fatalln(err)
 		}
 	},
-	Version: "v0.3.1",
+	Version: "v0.4.0-dev",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
