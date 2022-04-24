75719d0bce3e6099ceff0448290177fa4e41c9bc
Hannes Hayashi <26571811+hanneshayashi@users.noreply.github.com>
2022-04-24 07:06:24 +0200

    bump version

--- cmd/root.go
+++ cmd/root.go
@@ -133,7 +133,7 @@ For documentation see https://gsm.hayashi-ke.online.`,
 			log.Fatalln(err)
 		}
 	},
-	Version: "v0.5.0",
+	Version: "v0.6.0",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
