2c393c8d99a448b0c9e3797424a92bfd3e99f9d1
Hannes Hayashi <26571811+hanneshayashi@users.noreply.github.com>
2023-04-19 14:27:33 +0200

    update description

--- cmd/drivelabels_disable.go
+++ cmd/drivelabels_disable.go
@@ -30,10 +30,12 @@ import (
 var driveLabelsDisableCmd = &cobra.Command{
 	Use:   "disable",
 	Short: `Disables a Label`,
-	Long: `Permanently disables a Label and related metadata on Drive Items.
-Once disabled, the Label and related Drive item metadata will be disabled.
-Only draft Labels, and disabled Labels may be disabled.
+	Long: `Disable a published Label.
+Disabling a Label will result in a new disabled published revision based on the current published revision.
+If there is a draft revision, a new disabled draft revision will be created based on the latest draft revision.
+Older draft revisions will be deleted.
 
+Once disabled, a label may be deleted with "gsm driveLabels delete".
 Implements the API documented at https://developers.google.com/drive/labels/reference/rest/v2/labels/disable`,
 	DisableAutoGenTag: true,
 	Run: func(cmd *cobra.Command, _ []string) {
