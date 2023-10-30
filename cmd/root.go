1752997c95e6cc6d60c9ab93048f54add6a690c2
Hannes Hayashi <26571811+hanneshayashi@users.noreply.github.com>
2023-10-30 07:24:19 +0100

    update version

--- cmd/root.go
+++ cmd/root.go
@@ -141,7 +141,7 @@ For documentation see https://gsm.hayashi-ke.online.`,
 			log.Fatalln(err)
 		}
 	},
-	Version: "v0.10.0",
+	Version: "v0.11.0",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
